c300afd29cdf01c10f15b2f028fa5dd784b6d78b
tabrown3 <t-a-brown@hotmail.com>
2022-09-04 22:49:43 -0500

    fake_psx first draft

--- fake_psx/fake_psx.v
+++ fake_psx/fake_psx.v
@@ -8,7 +8,7 @@ module fake_psx(
         // ... low for the duration of transmission
 );
 
-    reg [7:0] buttons;
+    reg [23:0] data_store;
     reg [15:0] start_cmds
     integer byte_countdown;
 
@@ -23,9 +23,21 @@ module fake_psx(
             att <= 1'b0;
             start_cmds <= 16'h4201;
             byte_countdown <= 8;
-        end else if (start_cmds != 16'hxxxx && byte_countdown > 0) begin
-            cmd <= start_cmds[0];
-            start_cmds <= {1'bx, start_cmds[15:1]};
-            byte_countdown <= byte_countdown - 1;
+            data_store <= 24'hxxxxxx
+        end else if (byte_countdown > 0) begin
+            if (start_cmds != 16'hxxxx) begin
+                cmd <= start_cmds[0];
+                start_cmds <= {1'bx, start_cmds[15:1]};
+            end else begin
+                data_store <= {data, data_store}
+            end
+
+            if (data_store[0] == 1'bx) begin
+                byte_countdown <= byte_countdown - 1;
+            end else begin
+                att <= 1'b1;
+            end
+        end else if (!cmd) begin
+            cmd <= 1'b1;
         end
     end
