2f42bb5783b4418bbd265f668914e3b9f6b36bd7
tabrown3 <t-a-brown@hotmail.com>
2022-09-04 23:19:05 -0500

    fixed compile errors

--- fake_psx/fake_psx.v
+++ fake_psx/fake_psx.v
@@ -3,13 +3,13 @@ module fake_psx(
     input data, // serial data from controller
     input ack, // acknowledgement from controller
     output psx_clk, // clock the psx uses to drive the controller
-    output cmd, // psx uses cmd to command the controller to begin
+    output reg cmd, // psx uses cmd to command the controller to begin
     output att // psx should pull this low before commanding, and keep...
         // ... low for the duration of transmission
 );
 
     reg [23:0] data_store;
-    reg [15:0] start_cmds
+    reg [15:0] start_cmds;
     integer byte_countdown;
     reg wants_att = 1'b1;
 
@@ -25,13 +25,13 @@ module fake_psx(
             wants_att <= 1'b0;
             start_cmds <= 16'h4201;
             byte_countdown <= 8;
-            data_store <= 24'hxxxxxx
+            data_store <= 24'hxxxxxx;
         end else if (byte_countdown > 0) begin
             if (start_cmds != 16'hxxxx) begin
                 cmd <= start_cmds[0];
                 start_cmds <= {1'bx, start_cmds[15:1]};
             end else begin
-                data_store <= {data, data_store}
+                data_store <= {data, data_store};
             end
 
             if (data_store[0] == 1'bx) begin
@@ -43,3 +43,4 @@ module fake_psx(
             cmd <= 1'b1;
         end
     end
+endmodule
