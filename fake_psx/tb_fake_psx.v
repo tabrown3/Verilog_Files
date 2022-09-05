06c55629580615417518651b6e5e234535c46bea
tabrown3 <t-a-brown@hotmail.com>
2022-09-05 00:01:05 -0500

    started on testbench

--- fake_psx/tb_fake_psx.v
+++ fake_psx/tb_fake_psx.v
@@ -1,7 +1,22 @@
 `timescale 10us/100ns
 module tb_fake_psx();
 
+    // Testbench variables
     reg clk = 1;
+    reg data = 1;
+    reg ack = 1;
+    wire psx_clock;
+    wire cmd; // idle HIGH
+    wire att; // idle HIGH
+
+    fake_psx PSX(
+        .clk(clk),
+        .data(data),
+        .ack(ack),
+        .psx_clk(psx_clock),
+        .cmd(cmd),
+        .att(att)
+    );
 
     always begin
         #7.25; clk = ~clk; // about 7kHz
