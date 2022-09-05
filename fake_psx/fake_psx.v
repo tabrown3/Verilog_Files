12f0e3fa28d2d460cc4eb8416e47f6afc4525504
tabrown3 <t-a-brown@hotmail.com>
2022-09-05 10:30:08 -0500

    initialize cmd high

--- fake_psx/fake_psx.v
+++ fake_psx/fake_psx.v
@@ -3,13 +3,14 @@ module fake_psx(
     input data, // serial data from controller
     input ack, // acknowledgement from controller
     output psx_clk, // clock the psx uses to drive the controller
-    output reg cmd, // psx uses cmd to command the controller to begin
+    output cmd, // psx uses cmd to command the controller to begin
     output att // psx should pull this low before commanding, and keep...
         // ... low for the duration of transmission
 );
 
     // Internal variables
     reg wants_att = 1'b1;
+    reg out_cmd = 1'b1;
     integer byte_countdown;
     reg [15:0] start_cmds;
     reg [4:0] start_cmd_bits_sent;
@@ -18,6 +19,7 @@ module fake_psx(
 
     assign psx_clk = byte_countdown > 0 ? clk : 1'b1;
     assign att = wants_att;
+    assign cmd = out_cmd;
 
     always @(posedge ack) begin // resets byte countdown on ack
         byte_countdown <= 8;
@@ -33,14 +35,14 @@ module fake_psx(
             data_bits_received <= 5'h00;
         end else if (byte_countdown > 0) begin // this line waits for ack to reset the countdown
             if (start_cmd_bits_sent < 16) begin
-                cmd <= start_cmds[0];
+                out_cmd <= start_cmds[0];
                 start_cmds <= {1'b1, start_cmds[15:1]};
                 start_cmd_bits_sent <= start_cmd_bits_sent + 1'b1;
             end else if (data_bits_received < 24) begin
-                if (data_bits_received == 0 && !cmd) begin
+                if (data_bits_received == 0 && !out_cmd) begin
                     // when the command phase is over, set cmd HIGH...
                     // ... if not already
-                    cmd <= 1'b1;
+                    out_cmd <= 1'b1;
                 end
                 data_store <= {data, data_store};
                 byte_countdown <= byte_countdown - 1;
