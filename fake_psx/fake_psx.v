5262313bdf072d24df8290089da8bbd4738a5404
tabrown3 <t-a-brown@hotmail.com>
2022-09-07 21:10:42 -0500

    consolidate procedures

--- fake_psx/fake_psx.v
+++ fake_psx/fake_psx.v
@@ -33,39 +33,31 @@ module fake_psx(
         end else begin
             if (out_att) begin
                 out_att <= 1'b0;
+                byte_countdown <= 8;
+                start_cmds <= 16'h4201; // 0x01 - start, 0x42 - send data
+                start_cmd_bits_sent <= 5'h00;
+                data_store <= 24'h000000;
+                data_bits_received <= 5'h00;
             end
 
             if (byte_countdown > 0 && !out_att) begin
                 psx_clk <= 1'b0;
-            end
-        end
-    end
-
-    always @(negedge out_att) begin
-        byte_countdown <= 8;
-        start_cmds <= 16'h4201; // 0x01 - start, 0x42 - send data
-        start_cmd_bits_sent <= 5'h00;
-        data_store <= 24'h000000;
-        data_bits_received <= 5'h00;
-    end
-
-    always @(negedge psx_clk) begin
-        if (byte_countdown > 0) begin // this line waits for ack to reset the countdown
-            if (start_cmd_bits_sent < 16) begin
-                out_cmd <= start_cmds[0];
-                start_cmds <= {1'b1, start_cmds[15:1]};
-                start_cmd_bits_sent <= start_cmd_bits_sent + 1'b1;
-            end else if (data_bits_received < 24) begin
-                if (data_bits_received == 0 && !out_cmd) begin
-                    // when the command phase is over, set cmd HIGH...
-                    // ... if not already
-                    out_cmd <= 1'b1;
+                if (start_cmd_bits_sent < 16) begin
+                    out_cmd <= start_cmds[0];
+                    start_cmds <= {1'b1, start_cmds[15:1]};
+                    start_cmd_bits_sent <= start_cmd_bits_sent + 1'b1;
+                end else if (data_bits_received < 24) begin
+                    if (data_bits_received == 0 && !out_cmd) begin
+                        // when the command phase is over, set cmd HIGH...
+                        // ... if not already
+                        out_cmd <= 1'b1;
+                    end
+                    data_store <= {data, data_store};
+                    data_bits_received <= data_bits_received + 1'b1;
                 end
-                data_store <= {data, data_store};
-                data_bits_received <= data_bits_received + 1'b1;
-            end
 
-            byte_countdown <= byte_countdown - 1;
+                byte_countdown <= byte_countdown - 1;
+            end
         end
     end
 endmodule
