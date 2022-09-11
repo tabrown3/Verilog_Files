62e6d0505531e8d7f5cf9bc3012601e44a299670
tabrown3 <t-a-brown@hotmail.com>
2022-09-11 15:53:23 -0500

    second pass at controller

--- fake_psx/fake_controller.v
+++ fake_psx/fake_controller.v
@@ -4,8 +4,8 @@ module fake_controller
     // DATA1: SLCT           STRT UP   RGHT DOWN LEFT
     // DATA1: L2   R2    L1  R1   /\   O    X    |_|
     // source https://gamesx.com/controldata/psxcont/psxcont.htm#CIRCUIT
-    parameter FAKE_DATA1 = 8'b11111110, // Pressed left on d-pad
-    parameter FAKE_DATA2 = 8'b11111101 // Pressed X
+    parameter FAKE_DATA1 = 8'b01111111, // Pressed left on d-pad, reversed
+    parameter FAKE_DATA2 = 8'b10111111 // Pressed X, reversed
     // paremeters should be removed before emulating controller
 )(
     input psx_clk,
@@ -14,65 +14,32 @@ module fake_controller
     input clk, // this is a fake input to drive the (usually analog) ack
     // NOTE: if using FPGA to emulate controller, this'll be the onboard
     // ... clock; normally this would be governed by an RC circuit
-    output data,
+    output reg data,
     output ack
 );
 
-    reg [7:0] start_cmd;
-    reg [7:0] request_data_cmd;
-    reg out_ack = 1'b1;
-    reg [1:0] ack_dur;
-    reg [39:0] data_buffer = 40'hffffffffff;
-    integer byte_countdown;
-    reg out_data = 1'b1;
+    reg [7:0] data0;
+    reg [7:0] data1;
+    reg [7:0] data2;
+    reg [7:0] data3;
+    reg [7:0] data4;
 
-    assign ack = out_ack;
-    assign data = out_data;
-
-    // this happens before anything else
-    always @(negedge att) begin
-        start_cmd <= 8'h00;
-        request_data_cmd <= 8'hff;
-        out_ack <= 1'b1;
-        ack_dur <= 1'b0;
-        data_buffer <= {FAKE_DATA2, FAKE_DATA1, 8'h5a, 8'h41, 8'hff};
-        byte_countdown <= 40;
-    end
-
-    always @(posedge psx_clk) begin
-        // You could conceivably read the commands here, but... why?
-        // There're going to be 16 bits of them.
-        if (start_cmd != 8'h01) begin
-            start_cmd <= {cmd, start_cmd[7:1]};
-        end else if (request_data_cmd != 8'h42) begin
-            request_data_cmd <= {cmd, request_data_cmd[7:1]};
-        end
-    end
-
-    always @(negedge psx_clk) begin
-        // if start command has been received
-        if (start_cmd == 8'h01) begin
-            out_data <= data_buffer[40 - byte_countdown];
-        end
-
-        byte_countdown <= byte_countdown - 1;
-    end
-
-    always @(posedge psx_clk) begin
-        if (byte_countdown == 32 || byte_countdown == 24 ||
-            byte_countdown == 16 || byte_countdown == 8) begin
-            out_ack <= 1'b0;
-        end
-    end
-
-    always @(negedge clk) begin
-        if (out_ack == 1'b0) begin
-            ack_dur <= ack_dur + 1'b1;
-        end
-
-        if (ack_dur == 1'b1) begin
-            out_ack <= 1'b1;
-            ack_dur <= 1'b0;
+    always @(negedge psx_clk or negedge att)
+    begin: SHIFT_REGISTER // SISO w/ preload and async reset
+        if (!psx_clk) begin
+            data4 <= {1'b1, data4[7:1]};
+            data3 <= {data4[0], data3[7:1]};
+            data2 <= {data3[0], data2[7:1]};
+            data1 <= {data2[0], data1[7:1]};
+            data0 <= {data1[0], data0[7:1]};
+            data <= data0[0];
+        end else begin // acts as the register reset
+            data0 <= 8'hff;
+            data1 <= 8'h41;
+            data2 <= 8'h5a;
+            data3 <= FAKE_DATA1;
+            data4 <= FAKE_DATA2;
+            data = 1'b1;
         end
     end
 endmodule
---
+++ fake_psx/tb_fake_controller.v
@@ -0,0 +1,25 @@
+`timescale 1us/10ns
+module tb_fake_controller();
+    wire psx_clk, cmd, att;
+    wire data, ack;
+    reg clk = 0;
+
+    import_saleae_data PSX0(
+        .psx_clk(psx_clk),
+        .cmd(cmd),
+        .att(att)
+    );
+
+    fake_controller CONT(
+        .psx_clk(psx_clk),
+        .cmd(cmd),
+        .att(att),
+        .clk(clk),
+        .data(data),
+        .ack(ack)
+    );
+
+    always begin
+        #2; clk = ~clk; // 2us per toggle, 4us period (~250kHz)
+    end
+endmodule
