f19d98e7913554adcf91b2c36b239f379044be8c
tabrown3 <t-a-brown@hotmail.com>
2022-10-01 11:47:50 -0500

    start on t-rex

--- t_rex/t_rex.v
+++ t_rex/t_rex.v
@@ -1,3 +1,33 @@
-module t_rex();
+`include "controller/n64/n64_controller.v"
+`include "console/psx/psx_console.v"
+
+module t_rex(
+    input sample_clk,
+    input psx_data,
+    input psx_ack,
+    input n64_rx,
+    output n64_tx,
+    output reg psx_clk = 1'b1,
+    output reg psx_cmd = 1'b1,
+    output reg psx_att = 1'b1
+);
+
+    n64_controller CONTROLLER
+    (
+        .data_rx(n64_rx),
+        .sample_clk(sample_clk),
+        .btn(2'b00),
+        .data_tx(n64_tx)
+    );
+
+    psx_console #(.BOOT_TIME(10E4)) CONSOLE
+    (
+        .clk(sample_clk),
+        .data(psx_data),
+        .ack(psx_ack),
+        .psx_clk(psx_clk),
+        .cmd(psx_cmd),
+        .att(psx_att)
+    );
 
 endmodule
